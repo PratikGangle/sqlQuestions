d3d0bc9f1f0e824c5e1a249726bcf4891dc461a2
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2024-08-06 23:49:49 +0530

    Time: 680 ms (14.27%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -3,7 +3,7 @@ SELECT employee_id
 FROM  
   (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
    UNION 
-   SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
+   SELECT * FROM Salaries LEFT JOIN Employees  USING(employee_id))
 AS Temp
 WHERE salary IS NULL OR name IS NULL
 ORDER BY employee_id ASC
