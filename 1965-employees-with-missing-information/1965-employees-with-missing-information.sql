ac886ba00e9cb8fee032bca4559d13d7ba9783c6
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2024-08-06 23:49:05 +0530

    Time: 475 ms (85.31%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT employee_id
+FROM  
+  (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
+   UNION 
+   SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
+AS Temp
+WHERE salary IS NULL OR name IS NULL
+ORDER BY employee_id ASC
+
+
+
